e20df8c978c47f73dd2d445f20ed5764c6133c0a
Jugal Mistry <jugalmistry99@gmail.com>
2019-10-20 23:41:36 +0530

    User Login Signup

---
+++ server/routes/UserRoutes.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const userRoutes = express.Router();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+
+userRoutes.use(bodyParser.urlencoded({
+    extended: true
+}));
+const User = require('../models/User');
+userRoutes.use(cors());
+
+process.env.SECRET_KEY = 'key';
+
+userRoutes.post('/signup', (req, res) => {
+    const userData = {
+        email: req.body.email,
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        gender: req.body.gender,
+        dob: req.body.dob,
+        password: req.body.password,
+    }
+
+    bcrypt.hash(req.body.password, 10, (error, hashed) => {
+        if (error) {
+            res.json({
+                status: 'User Insert Error'
+            });
+        } else {
+            userData.password = hashed;
+            User.create(userData)
+                .then(user => {
+                    res.json({
+                        status: 'ok'
+                    });
+                })
+                .catch(err => {
+                    res.json({
+                        status: err
+                    });
+                });
+        }
+    });
+});
+
+userRoutes.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(user => {
+        if (user) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+                    expiresIn: 1440
+                })
+                res.send(token);
+            }
+        } else {
+            res.status(400).json({ error: 'No Such User' });
+        }
+    })
+});
